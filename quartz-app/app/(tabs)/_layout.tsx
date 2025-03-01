38ceffbc0658129a95e574e051360f4d77e8bdab
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-02-28 21:07:37 -0700

    Add tx history tab

--- quartz-app/app/(tabs)/_layout.tsx
+++ quartz-app/app/(tabs)/_layout.tsx
@@ -115,6 +115,12 @@ export default function TabLayout() {
             tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
           }}
         />
+        <Tabs.Screen
+          name="txHistory"
+          options={{
+            title: 'History',
+          }}
+        />
       </Tabs>
     );
   }
---
+++ quartz-app/app/(tabs)/txHistory.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function TxHistoryPage() {
+
+  return (
+    <View>
+      <Text>Transaction History Page!</Text>
+    </View>
+  );
+}
