2c77af177753dd22ca19fcd50d70b380a5cb3392
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-03-07 08:33:34 +0000

    add spendlimit page to app folder

--- quartz-app/app/spendLimit.tsx
+++ quartz-app/app/spendLimit.tsx
@@ -1,9 +1,9 @@
 //add a page that opens the AddFundsPage component
 
 import { View, Text } from "react-native";
-
+import SpendLimitsPage from "@/components/PageVariations/SpendLimit";
 export default function SpendLimitPageWrapper() {
     return <View>
-        <Text>Spend Limit!!!!</Text>
+        <SpendLimitsPage />
     </View>;
 }
