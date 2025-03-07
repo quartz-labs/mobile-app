eaf202103075849c76b40b62069f50491db059d9
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-03-07 15:40:17 +0000

    Remove unused import

--- quartz-app/app/spendLimit.tsx
+++ quartz-app/app/spendLimit.tsx
@@ -1,6 +1,6 @@
 //add a page that opens the AddFundsPage component
 
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import SpendLimitsPage from "@/components/PageVariations/SpendLimit";
 export default function SpendLimitPageWrapper() {
     return <View>
